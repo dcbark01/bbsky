9ae8fdb26136e1716e610b6cc1288bf5385cb55b
dcbark01 <dcbark01@gmail.com>
2024-10-27 09:27:42 -0500

    Improved functionality of config update CLI

--- src/bbsky/config.py
+++ src/bbsky/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import Any, Callable
 
 import click
-from attrs import define
+from attrs import define, evolve
 
 from .data_cls import URL, structure, unstructure
 from .paths import BBSKY_CONFIG_DIR, BBSKY_CONFIG_FILE
@@ -95,16 +95,24 @@ def cli():
 @click.option("--client-secret", prompt="Client Secret")
 @click.option("--redirect-uri", prompt="Redirect URI")
 @click.option("--subscription-key", prompt="Subscription Key")
-@click.option("--output", type=click.Path(), default=BBSKY_CONFIG_FILE)
+@click.option("--output-path", type=click.Path(), default=BBSKY_CONFIG_FILE)
 @click.pass_context
 @ensure_config_dir
 def create(
-    context: click.Context, client_id: str, client_secret: str, redirect_uri: str, subscription_key: str, output: Path
+    context: click.Context,
+    client_id: str,
+    client_secret: str,
+    redirect_uri: str,
+    subscription_key: str,
+    output_path: Path,
 ) -> None:
     """Create a new Blackbaud Sky API config."""
+    output_path = Path(output_path)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
     config = SkyConfig(client_id, client_secret, URL(redirect_uri), subscription_key)
-    config.to_json_file(output)
-    click.echo(f"Config saved to {output}")
+    config.to_json_file(output_path)
+    click.echo(f"Config saved to {output_path}")
 
 
 @click.command()
@@ -118,22 +126,36 @@ def show(
 ) -> None:
     """Show the current Blackbaud Sky API config."""
     input_path = Path(input_path)
+    if not input_path.exists():
+        click.echo(f"Config not found at {input_path}.")
+        return
     config = SkyConfig.from_json_file(input_path)
+    _show(config, fmt)
+
+
+def _show(config: SkyConfig, fmt: str) -> None:
+    """Show the current Blackbaud Sky API config.
+
+    Format options:
+    - json: JSON format
+    - env: Environment variables
+
+    """
     if fmt == "json":
         click.echo(json.dumps(config.to_dict(), indent=4))
     elif fmt == "env":
-        click.echo(f"export BLACKBAUD_CLIENT_ID={config.client_id}")
-        click.echo(f"export BLACKBAUD_CLIENT_SECRET={config.client_secret}")
-        click.echo(f"export BLACKBAUD_REDIRECT_URI={config.redirect_uri}")
-        click.echo(f"export BLACKBAUD_SUBSCRIPTION_KEY={config.subscription_key}")
+        click.echo(f"export {SkyConfigEnvVars.CLIENT_ID.value}={config.client_id}")
+        click.echo(f"export {SkyConfigEnvVars.CLIENT_SECRET.value}={config.client_secret}")
+        click.echo(f"export {SkyConfigEnvVars.REDIRECT_URI.value}={config.redirect_uri}")
+        click.echo(f"export {SkyConfigEnvVars.SUBSCRIPTION_KEY.value}={config.subscription_key}")
 
 
 @click.command()
 @click.option("-i", "--input_path", type=click.Path(), default=BBSKY_CONFIG_FILE)
-@click.option("--client-id", prompt="Client ID")
-@click.option("--client-secret", prompt="Client Secret")
-@click.option("--redirect-uri", prompt="Redirect URI")
-@click.option("--subscription-key", prompt="Subscription Key")
+@click.option("--client-id", prompt="Client ID (leave blank to keep current value)", default="")
+@click.option("--client-secret", prompt="Client Secret (leave blank to keep current value)", default="")
+@click.option("--redirect-uri", prompt="Redirect URI (leave blank to keep current value)", default="")
+@click.option("--subscription-key", prompt="Subscription Key (leave blank to keep current value)", default="")
 @click.pass_context
 @ensure_config_dir
 def update(
@@ -145,10 +167,41 @@ def update(
     subscription_key: str,
 ) -> None:
     """Update the current Blackbaud Sky API config."""
+
+    if not any([client_id, client_secret, redirect_uri, subscription_key]):
+        click.echo("No new values provided. Exiting.")
+        return
+
+    # Load the original config
     input_path = Path(input_path)
-    config = SkyConfig(client_id, client_secret, URL(redirect_uri), subscription_key)
-    config.to_json_file(input_path)
-    click.echo(f"Config updated at {input_path}")
+    input_path.parent.mkdir(parents=True, exist_ok=True)
+    config_orig = SkyConfig.from_json_file(input_path)
+
+    # Show the original config
+    click.echo("\nCurrent SkyConfig:")
+    _show(config_orig, "json")
+
+    # Update with new values
+    config_updated = evolve(
+        config_orig,
+        **{
+            "client_id": client_id if client_id else config_orig.client_id,
+            "client_secret": client_secret if client_secret else config_orig.client_secret,
+            "redirect_uri": URL(redirect_uri) if redirect_uri else config_orig.redirect_uri,
+            "subscription_key": subscription_key if subscription_key else config_orig.subscription_key,
+        },
+    )
+
+    # Show the updated config
+    click.echo("\nUpdated SkyConfig:")
+    _show(config_updated, "json")
+
+    # Confirm user wants to save the updated config
+    if click.confirm("Save the updated config?"):
+        config_updated.to_json_file(input_path)
+        click.echo(f"Config updated and saved to {input_path}")
+    else:
+        click.echo("Config not saved.")
 
 
 @click.command()
